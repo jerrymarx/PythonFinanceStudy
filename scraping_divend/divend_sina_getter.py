e2a888566cb745ede202023ff424624fcd3c0ad7
Jerry Marx <jerrymarx@gmail.com>
2018-08-16 15:17:48 -0400

    Get the prices as group, that will make it faster.

--- scraping_divend/divend_sina_getter.py
+++ scraping_divend/divend_sina_getter.py
@@ -1,4 +1,7 @@
-from urllib2 import urlopen
+try:
+	from urllib.request import urlopen
+except:
+	from urllib2 import urlopen
 from bs4 import BeautifulSoup as soup
 import re, datetime, csv, time, random
 
@@ -7,12 +10,13 @@ class divend_info:
 		self.full_dict = self.load_full_list(full_list_filename)
 		self.ah_dict = self.load_ah_list(ah_list_filename)
 		self.divend_data = self.load_divend_data(divend_filename)
+		self.price_dict = {}
 		self.usd = None
 		self.hkd = None
 		
 	def load_full_list(self, filename):
 		full_dict = {}
-		with open(filename) as csvfile:
+		with open(filename, encoding='UTF-8') as csvfile:
 			reader = csv.reader(csvfile)
 			for row in reader:
 				key = row[0]
@@ -52,53 +56,92 @@ class divend_info:
 				record[year] += volume * rate / 10
 		return divend_data
 
-	def get_last_price(self, code):
+	def get_code_str_for_sina_hq(self, code, prefix=''):
 		if len(code) == 5:
-			code = 'hk' + code
-			the_url = 'http://hq.sinajs.cn/format=text&list=' + code
-			client = urlopen(the_url)
-			print 'try to get', the_url
-			text = client.read()
-			client.close()
-			parts = text.split(',')
-			name = parts[1]
-			price_str = parts[6]
-			return float(price_str), name
-			
+			return 'hk' + code
+		if code[0] == '0' or code[0] == '2' or code[0] == '3':
+			return prefix + 'sz' + code
+		elif code[0] == '6' or code[0] == '9':
+			return prefix + 'sh' + code
 		else:
-			if code[0] == '0' or code[0] == '2' or code[0] == '3':
-				code = 'sz' + code
-			elif code[0] == '6' or code[0] == '9':
-				code = 'sh' + code
-			else:
-				print 'Unkonw code:', code
-				return None
+			print('Unknow code:', code)
+			return None
+		
+	def get_latest_price_from_sina(self):
+		total = len(self.full_dict) + len(self.ah_dict)
+		group_size = 50
+		done = 0
+		
+		key_list = list(self.full_dict.keys())
+		for i in range(0, len(self.full_dict), group_size):
+			temp_list = key_list[i:i+group_size]
+			code_list = [self.get_code_str_for_sina_hq(code, 's_') for code in temp_list]
 			
-			the_url = 'http://hq.sinajs.cn/format=text&list=s_' + code
+			the_url = 'http://hq.sinajs.cn/format=text&list=' + ','.join(code_list)
 			client = urlopen(the_url)
-			print 'try to get', the_url
+			print('try to get', the_url)
 			text = client.read()
 			client.close()
 			
-			code_name, price_str, delta, delta_percent, volume, turnover = text.split(',')
-			if price_str == '0.00':
-				the_url = 'http://hq.sinajs.cn/format=text&list=' + code
-				client = urlopen(the_url)
-				print 'try to get', the_url
-				text = client.read()
-				client.close()
-				price_str = text.split(',')[2]
+			lines = text.decode('gbk').split('\n')
+			for line in lines:
+				if line.strip() == '':
+					continue
+				parts = line.split(',')
+				code, name = parts[0].split('=')
+				price_str = parts[1]
 				
-			scode, name = code_name.split('=')
-			if scode[-8:] != code:
-				print 'Some thing error, request', code, ', but return', scode
-			price = float(price_str)
+				if price_str == '0.00':
+					the_url = 'http://hq.sinajs.cn/format=text&list=' + code[-8:]
+					client = urlopen(the_url)
+					print('try to get', the_url)
+					text = client.read()
+					client.close()
+					price_str = text.decode('gbk').split(',')[2]
+					name = name + ' S'
+				
+				self.price_dict[code[-6:]] = [name, float(price_str),]
+				done += 1
+			print('Get info of [%d/%d] done.' % (done, total))
+				
+		key_list = list(self.ah_dict.keys())
+		for i in range(0, len(self.ah_dict), group_size):
+			temp_list = key_list[i:i+group_size]
+			code_list = [self.get_code_str_for_sina_hq(code) for code in temp_list]
 			
-			return price, name
+			the_url = 'http://hq.sinajs.cn/format=text&list=' + ','.join(code_list)
+			client = urlopen(the_url)
+			print('try to get', the_url)
+			text = client.read()
+			client.close()
+			
+			lines = text.decode('gbk').split('\n')
+			for line in lines:
+				if line.strip() == '':
+					continue
+				parts = line.split(',')
+				code = parts[0].split('=')[0][-5:]
+				name = parts[1]
+				price_str = parts[6]
+				self.price_dict[code] = [name, float(price_str),]
+				done += 1
+			print('Get info of [%d/%d] done.' % (done, total))
+		
+	def get_last_price(self, code):
+		if code in self.price_dict:
+			info = self.price_dict[code]
+			return info[1], info[0]
+		else:
+			print('Cannot find price and name for code:', code)
+			return None, None
 
 	def format_row(self, code, price, name):
-		items = ['="' + code + '"', str(price),]
-		key = self.ah_dict[code]['code_a'] if len(code) == 5 else code
+		items = ['="' + code + '"', name, str(price),]
+		key = code
+		if len(code) == 5:
+			key = self.ah_dict[code]['code_a']
+		elif self.full_dict[code]['code_a'] != '':
+			key = self.full_dict[code]['code_a']
 		common_info = self.full_dict[key]
 		total = common_info['total_a'] + common_info['total_b']
 		total_value = total * price
@@ -122,37 +165,42 @@ class divend_info:
 		client = urlopen(the_url)
 		text = client.read()
 		client.close()
-		lines = text.split('\n')
+		lines = text.decode('gbk').split('\n')
 		self.usd = float(lines[0].split(',')[2])
 		self.hkd = float(lines[1].split(',')[2])
 
 	def get_data_from_sina_then_write(self, code):
 		price, name = self.get_last_price(code)
 		text = self.format_row(code, price, name)
-		with open('output.csv', 'a') as ofile:
+		with open('output.csv', 'a', encoding='utf-8-sig') as ofile:
 			ofile.write(text)
 			ofile.write('\n')
 		
 	def main(self):
 		self.get_usd_hkd()
-		with open('output.csv', 'w') as ofile:
-			heads = ['CODE', 'PRICE', 'TOTAL', 'MARKET']
+		with open('output.csv', 'w',encoding='utf-8-sig') as ofile:
+			heads = ['CODE', 'NAME', 'PRICE', 'TOTAL', 'MARKET']
 			for year in range(2017, 1990, -1):
 				heads.append(str(year))
 				heads.append(str(year)+'p')
 			ofile.write(','.join(heads))
 			ofile.write('\n')
-		total = len(self.full_dict) + len(self.ah_dict)
+		self.get_latest_price_from_sina()
 		done = 0
+		print('Handle the data', end='', flush=True)
 		for key in self.full_dict:
 			self.get_data_from_sina_then_write(key)
 			done += 1
-			print 'Get info of %s done. [%d/%d]' % (key, done, total)
+			if done % 100 == 0:
+				print('.', end='', flush=True)
+				
 		for key in self.ah_dict:
 			self.get_data_from_sina_then_write(key)
 			done += 1
-			print 'Get info of %s done. [%d/%d]' % (key, done, total)
+			if done % 100 == 0:
+				print('.', end='', flush=True)
 
+		print(' Done')
 
 if __name__ == '__main__':
 	worker = divend_info('full_list.csv', 'ah.csv', 'divend.csv');
