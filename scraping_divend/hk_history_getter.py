ee07e2a228a3dc5c596c8bba6673931678357fe9
Jerry Marx <jerrymarx@gmail.com>
2018-08-23 22:15:47 -0400

    handle it no response / no timeout issue of urlopen and read issues for hk_history_getter.py

--- scraping_divend/hk_history_getter.py
+++ scraping_divend/hk_history_getter.py
@@ -1,16 +1,33 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
+from threading import Timer
+from datetime import datetime
 import re, urllib, time, random, csv, os
 
 def get_HK_stock_history_range(code):
 	the_url = 'http://stock.finance.sina.com.cn/hkstock/api/jsonp.php//HistoryTradeService.getHistoryRange?symbol=' + code
-	print('Get history range for', code, 'from', the_url)
-	client = urlopen(the_url)
-	text = client.read().decode('gbk')
-	client.close()
+	print('Get history range for', code, 'from', the_url, end='', flush=True)
+	for i in range(10):
+		client = None
+		try:
+			print('.', end='', flush=True)
+			client = urlopen(the_url, timeout=10)
+			t = Timer(10, close_response, [client, ])
+			t.start()
+			text = client.read().decode('gbk')
+			t.cancel()
+			client.close()
+			break
+		except:
+			if client:
+				client.close()
+	else:
+		print('Failed')
+		return None, 0, 0, 0
 
 	rp = re.compile('\(\(\{max:\"(.*)-(.*)\",min:\"(.*)-(.*)\".*')
 	matches = re.findall(rp, text)
+	print('Done')
 	return matches[0]
 	
 def x_history_range(max_year, max_season, min_year, min_season):
@@ -27,29 +44,35 @@ def x_history_range(max_year, max_season, min_year, min_season):
 		yield y, s
 	yield max_y, max_s
 
+def close_response(client):
+	client.close()
+	
 def get_HK_stock_history(code, year, season):
 	the_url = 'http://stock.finance.sina.com.cn/hkstock/history/' + code + '.html'
 	values = {'year': str(year), 'season':str(season)}
 	data = urllib.parse.urlencode(values).encode('utf-8')
 	req = urllib.request.Request(the_url, data)
 	print('Try to get history data of', code, ',', year, 'year', season, 'season', end=' ', flush=True)
-	success = False
 	for i in range(5):
 		print('>', end='', flush=True)
 		client = None
 		try:
 			client = urlopen(req, timeout=10)
 			print('.', end='', flush=True)
-			page_soup = soup(client.read(), 'html.parser')
+			t = Timer(10, close_response, [client, ])
+			t.start()
+			content = client.read()
+			t.cancel()
+			print('.', end='', flush=True)
+			page_soup = soup(content, 'html.parser')
 			print('.', end='', flush=True)
 			client.close()
-			success = True
 			break
 		except:
 			if client:
 				client.close()
 			continue
-	if not success:
+	else:
 		print('Failed')
 		return None
 	print('Done')
@@ -80,11 +103,13 @@ def get_HK_stock_history(code, year, season):
 	
 def get_HK_stock_whole_history(code):
 	max_year, max_season, min_year, min_season = get_HK_stock_history_range(code)
+	if max_year is None:
+		return None
 	history = list()
 	for year, season in x_history_range(max_year, max_season, min_year, min_season):
 		# print('year:', year, 'season:', season)
 		lines = get_HK_stock_history(code, year, season)
-		if lines == None:
+		if lines is None:
 			break
 		# print('Get', len(lines), 'lines data')
 		history.extend(lines)
@@ -101,6 +126,17 @@ def save_to_file(code, history):
 			csvfile.write(line)
 			csvfile.write('\n')
 
+def get_latest_date_from_file(code):
+	last_line = None
+	with open(history_data_filename(code)) as csvfile:
+		reader = csv.reader(csvfile)
+		lines = list(reader)
+		for i in range(-1,-1 - max(5, len(lines)),-1):
+			if len(lines[i]) != 0:
+				last_line = lines[i]
+				break
+	return datetime.strptime(last_line[0], '%Y%m%d').date()
+			
 def load_hk_stock_full_list(filename):
 	dict = {}
 	with open(filename, encoding='utf-8') as csvfile:
@@ -125,15 +161,19 @@ if __name__ == '__main__':
 #	history = get_HK_stock_whole_history(code)
 #	save_to_file(code, history)
 
-	hk_stock = load_hk_stock_full_list('list.hk.csv')
-	total = len(hk_stock)
-	done = 0
-	for code in hk_stock:
-		if os.path.exists(history_data_filename(code)):
-			done += 1
-			print('History data for', code, hk_stock[code], 'already exist. [%d/%d] Done.' % (done, total))
-		else:
-			data = get_HK_stock_whole_history(code)
-			save_to_file(code, data)
-			done += 1
-			print('Got history data for', code, hk_stock[code], '[%d/%d] Done.' % (done, total))
+#	hk_stock = load_hk_stock_full_list('list.hk.csv')
+#	total = len(hk_stock)
+#	done = 0
+#	for code in hk_stock:
+#		if os.path.exists(history_data_filename(code)):
+#			done += 1
+#			print('History data for', code, hk_stock[code], 'already exist. [%d/%d] Done.' % (done, total))
+#		else:
+#			data = get_HK_stock_whole_history(code)
+#			if data is None:
+#				continue
+#			save_to_file(code, data)
+#			done += 1
+#			print('Got history data for', code, hk_stock[code], '[%d/%d] Done.' % (done, total))
+
+	print(get_latest_date_from_file('00038'))
